5fe00747a9d317679009faadd19704e871ad08b0
Task View <maumauriciog@hotmail.com>
2025-02-02 18:11:37 -0300

    alterando arquivos main.css e script.css - ver 1.2

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
+    var nrTarefa = 0;
 
     $('form').on('submit', function (e) {
         e.preventDefault();
         var tarefa = $('#tarefa').val();
-        
+
         console.log(tarefa);
-        $('#lista-tarefas').append(`<li>${tarefa}</li>`);
+        $('#lista-tarefas').append(`<li id="nr${nrTarefa}">${tarefa}</li>`);
+
+        nrTarefa +=1;
+        console.log(nrTarefa);
     })
 })
 
