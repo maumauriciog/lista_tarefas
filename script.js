45f940c31f9d8ca155d72d64d53440a56bf8eabb
Task View <maumauriciog@hotmail.com>
2025-02-03 09:58:06 -0300

    atualização arquivos script.js, main.css e index.html - v.1.3

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 $(document).ready(function () {
-    var nrTarefa = 0;
+    let valor = 1;
 
     $('form').on('submit', function (e) {
         e.preventDefault();
+        valor += 1;
         var tarefa = $('#tarefa').val();
 
-        $('#lista-tarefas').append(`<li id="#nr${nrTarefa}">${tarefa}</li>`);
-        nrTarefa +=1;
+        if (tarefa === '') {
+            alert('Digite uma tarefa.');
+        } else {
+            var itemTarefa = $(`<li>${tarefa}</li>`);
+            $(itemTarefa).appendTo('ul');
+        }
+        
+        $('#taskList').on('click', function () {
+            $('li').toggleClass('done');
+        });
     })
+
 })
 
